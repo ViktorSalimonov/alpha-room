00ce4e32451a4ccddfed0dca049b9418c6f50193
Viktor Salimonov <viktor.salimonov@outlook.com>
2020-07-07 21:20:00 +0300

    Add simple home page

--- alpha/views.py
+++ alpha/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    """Home page template view."""
+
+    template_name = 'home.html'
